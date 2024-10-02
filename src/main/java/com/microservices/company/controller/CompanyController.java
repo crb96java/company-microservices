222f921f2f6d5bcfdb2e5f10ca637e6267a544a4
crb96java <crb96java@gmail.com>
2024-10-02 21:13:59 +0530

    Get the company details based on company name & company email.

--- src/main/java/com/microservices/company/controller/CompanyController.java
+++ src/main/java/com/microservices/company/controller/CompanyController.java
@@ -2,15 +2,15 @@ package com.microservices.company.controller;
 
 import com.microservices.company.model.CompanyDetails;
 import com.microservices.company.service.CompanyService;
+import com.microservices.company.util.Constants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -39,4 +39,26 @@ public class CompanyController {
         companyService.saveCompanyDetails(companyDetails);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    /**
+     * Request POST call to create Company Details
+     * @param companyDetails
+     *      - the company to create
+     * @return no return type
+     */
+    @GetMapping("/company/{companyName}/{companyEmail}")
+    public ResponseEntity<CompanyDetails> getCompanyDetailsByCompanyNameAndCompanyEmail(@PathVariable final String companyName, @PathVariable final String companyEmail) {
+        log.info("CompanyController::getCompanyDetailsByCompanyNameAndCompanyEmail request received with {} : {}", companyName, companyEmail);
+
+        try{
+            //Get the company details by company name and company email
+            CompanyDetails companyDetails = companyService.getCompanyDetailsByNameAndEmail(companyName, companyEmail);
+            return new ResponseEntity<>(companyDetails, HttpStatus.OK);
+        }catch (Exception exception){
+            log.error("CompanyController::getCompanyDetailsByCompanyNameAndCompanyEmail failing with Error message {}", exception.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
 }
